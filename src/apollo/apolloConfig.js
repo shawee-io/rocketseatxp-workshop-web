6357a312821ce39e9df40f98cab1c92adcc80b70
Wendel Nascimento <wendel.nascimento2@outlook.com>
2019-11-22 15:50:34 -0300

    feat: configuring apollo

---
+++ src/apollo/apolloConfig.js
@@ -0,0 +1,25 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createUploadLink } from 'apollo-upload-client';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    createUploadLink({
+      uri: 'http://localhost:4001'
+    }),
+  ]),
+  cache: new InMemoryCache()
+});
+
+export default client;
